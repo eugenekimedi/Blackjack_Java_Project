0294e43c5cdea134f8f05633d66529bd0aaa7c12
Eugene Kim <eugenekim.edi@gmail.com>
2017-07-07 11:41:00 +0100

    Passed Deck deckStartsEmpty

--- app/src/main/java/com/example/eugenekim/blackjackproject/Deck.java
+++ app/src/main/java/com/example/eugenekim/blackjackproject/Deck.java
@@ -5,4 +5,21 @@ package com.example.eugenekim.blackjackproject;
  */
 
 public class Deck {
+
+    private Card[] cards;
+
+    public Deck() {
+        this.cards = new Card[52];
+    }
+
+    public int cardCount() {
+        int numberOfCards = 0;
+
+        for(Card card : this.cards) {
+            if (card != null) {
+                numberOfCards++;
+            }
+        }
+        return numberOfCards;
+    }
 }
---
+++ app/src/test/java/com/example/eugenekim/blackjackproject/DeckTest.java
@@ -0,0 +1,26 @@
+package com.example.eugenekim.blackjackproject;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by eugenekim on 7/7/17.
+ */
+
+public class DeckTest {
+
+    Deck deck;
+    Card card;
+
+    @Before
+    public void before() {
+        deck = new Deck();
+        card = new Card(Rank.TWO, Suit.CLUBS);
+    }
+
+    @Test
+    public void deckStartsEmpty() {
+        assertEquals(0, deck.cardCount());
+    }
+}
